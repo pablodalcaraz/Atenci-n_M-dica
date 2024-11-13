7e220b5f2c72917b1ff809feea660a5f509017a7
Pablo Damián Alcaraz <pablodalcaraz@gmail.com>
2024-11-13 17:10:53 -0300

    Cambio connection por pool

--- src/database/db.js
+++ src/database/db.js
@@ -1,12 +1,15 @@
-import mysql from 'mysql2/promise'
-import dotenv from 'dotenv'
-dotenv.config()
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const connexion = mysql.createConnection({
+const connexion = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10, 
+    queueLimit: 0        
+});
 
-export default connexion
+export default connexion;
